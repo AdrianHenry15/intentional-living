02aa933da4ac1faabf988ad5ef32c6744607a526
Adrian <adrianhenry2115@gmail.com>
2025-02-23 16:23:23 -0500

    admin check

--- lib/check-admin.ts
+++ lib/check-admin.ts
@@ -2,8 +2,8 @@ export const isAdmin = (email: string): boolean => {
   const adminEmails = [
     process.env.NEXT_PUBLIC_AHENRY_PERSONAL,
     process.env.NEXT_PUBLIC_AHENRY_TGS,
-    process.env.NEXT_PUBLIC_JARED,
-    process.env.NEXT_PUBLIC_JAMES,
+    process.env.NEXT_PUBLIC_SAHIL,
+    process.env.NEXT_PUBLIC_RYLAN,
   ]
 
   return adminEmails.includes(email)
