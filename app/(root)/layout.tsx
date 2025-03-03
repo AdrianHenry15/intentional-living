f80ea5035fe9116c7aa4b57297a53388d70c945f
Adrian <adrianhenry2115@gmail.com>
2025-03-03 16:37:30 -0500

    to quiz

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,8 @@ import WeekCalendar from "@/components/week-calendar"
 import BottomNavbar from "@/components/navbar/bottom-navbar"
 import { useUser } from "@clerk/nextjs"
 import SettingsWidget from "@/components/settings-widget"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function RootLayout({
   children,
@@ -13,6 +14,13 @@ export default function RootLayout({
 }) {
   const { isSignedIn } = useUser()
   const pathname = usePathname()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard") // Change this to your valid route
+    }
+  }, [isSignedIn, router])
 
   // Define base routes where the calendar should be hidden
   const hiddenRoutes = ["/auth/notes", "/auth/referral", "/auth/profile"]
